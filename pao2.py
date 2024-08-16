b315f465299f61586b34e84b5e3ed6eebb8bde2c
mateus do skate <128601265+Mateus-Hideki@users.noreply.github.com>
2024-08-16 09:17:19 -0300

    Add files via upload

---
+++ pao2.py
@@ -0,0 +1,12 @@
+Alunos  = [
+    {"nome":"Igor", "Idade":20, "prontuario":"SP12345", "notas portugues":6, "notas matematica":8, "notas ciencias":10, "media":8},
+    {"nome":"ana", "Idade":19, "prontuario":"SP45678", "notas portugues":10, "notas matematica":3, "notas ciencias":9, "media":7.3}
+]
+
+def get_info():
+    return Alunos
+
+print(Alunos)
+
+def add_aluno():
+    
